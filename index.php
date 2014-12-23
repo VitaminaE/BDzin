8c78313aee8146442c10851f4961ce74745af43f
VitaminaE <estevan.gnm@gmail.com>
2014-12-23 21:50:13 -0200

    Create index.php
    pagina inicial do BD

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bootstrap Example</title>
+		<!-- Include meta tag to ensure proper rendering and touch zooming -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Include bootstrap stylesheets -->
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	</head>
+
+	<style>
+		body {
+			background-color: #29A5D4;
+		}
+	</style>
+
+	<body>
+		<div class="container theme-showcase" role="main">
+			<div class="header">
+				<h1>Home</h1>
+			</div>               
+		</div>
+
+		<ul>
+
+		</ul>
+
+		<!-- JavaScript placed at the end of the document so the pages load faster -->
+		<!-- Optional: Include the jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<!-- Optional: Incorporate the Bootstrap JavaScript plugins -->
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</body>
+
+</html>
+
+
+
+
+				
