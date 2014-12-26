924a53c4ba6d3ded8a6fd5777549cb14835f3d92
VitaminaE <estevan.gnm@gmail.com>
2014-12-26 20:58:25 -0200

    Update index.php
    busca simples implementada.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Bootstrap Example</title>
+		<title>Banco de Dados</title>
 		<!-- Include meta tag to ensure proper rendering and touch zooming -->
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Include bootstrap stylesheets -->
@@ -12,18 +12,63 @@
 		body {
 			background-color: #29A5D4;
 		}
+		.container {
+			background-color: #f2f2f2;
+		}
 	</style>
 
 	<body>
 		<div class="container theme-showcase" role="main">
 			<div class="header">
 				<h1>Home</h1>
+			</div>
+			<div>
+				<form method="post" action="index.php">
+					<label>Buscar por : </label>
+					<select name="item">
+						<option value="fonte">Fontes</option>
+						<option value="hd">HDs</option>
+						<option value="dvd">DVDs</option>
+						<option value="placaMae">Placas Mae</option>
+					</select>
+					<input type="submit" value="Enviar"/>
+				</form>
+			</div>
+			<br/>
+			<div id="result" >
+				<?php
+				session_start();
+
+				$table = $_POST["item"];
+
+				$dbLink = mysql_connect('localhost', 'root', 'root');
+
+				mysql_select_db('Manutencao', $dbLink); // Seleciona o BD da manutencao
+
+				// Realiza a consulta, no caso de erro fecha a conexao
+				$sql = "SELECT * FROM " .$table ;
+				$result = mysql_query($sql) or die(mysql_error()); 
+
+				// Imprime os nomes das colunas como o cabecalho da tabela
+				echo "<table class=\"table table-stripped table-bordered\"><tr>";
+				for($i = 0; $i < mysql_num_fields($result); $i++) {
+				    $field_info = mysql_fetch_field($result, $i);
+				    echo "<th>" .$field_info->name. "</th>"; // Acessa o campo nome em field_info
+				}
+
+				// Imprime os dados na tabela
+				while($row = mysql_fetch_row($result)) {
+				    echo "<tr>";
+				    foreach($row as $_column) {
+				        echo "<td>" .$_column. "</td>";
+				    }
+				    echo "</tr>";
+				}
+
+				echo "</table>";
+				?>
 			</div>               
 		</div>
-
-		<ul>
-
-		</ul>
 
 		<!-- JavaScript placed at the end of the document so the pages load faster -->
 		<!-- Optional: Include the jQuery library -->
@@ -33,8 +78,3 @@
 	</body>
 
 </html>
-
-
-
-
-				
